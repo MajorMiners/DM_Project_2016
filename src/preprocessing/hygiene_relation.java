0640d13f62c9b49df3a6b9fd9c658281f6466549
neeleshganan <ganan.neelesh@gmail.com>
2016-04-07 11:39:06 -0400

    modifying project structure

--- src/preprocessing/hygiene_relation.java
+++ src/preprocessing/hygiene_relation.java
@@ -3,10 +3,11 @@ package preprocessing;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import com.csvreader.CsvWriter;
 
@@ -15,143 +16,82 @@ import parser.ReviewParser;
 
 public class hygiene_relation {
 	
-	public static Set<String> hs = new HashSet<String>();
+	public static HashSet<String> hygieneDictionary = new HashSet<String>();
 	
-	public static void main(String[] args) throws IOException {
-		
-		
-		//add elements to set
-		hs.add("mess");
-		hs.add("dirty");
-		hs.add("table");
-		hs.add("rat");
-		hs.add("horrible");
-		hs.add("awful");
-		hs.add("raw");
-		hs.add("bathroom");
-		hs.add("hygiene");
-		hs.add("ill");
-		hs.add("bad");
-		hs.add("cheap");
-		hs.add("dark");
-		hs.add("floor");
-		hs.add("pleasant");
-		hs.add("hair");
-		hs.add("wash");
-		hs.add("paper");
-		hs.add("soap");
-		hs.add("cold");
-		hs.add("gross");
-		hs.add("filthy");
-		hs.add("crappy");
-		hs.add("sanitation");
-		hs.add("regimen");
-		hs.add("healthy");
-		hs.add("wholesome");
-		hs.add("foul");
-		hs.add("heaven");
-		hs.add("big");
-		hs.add("large");
-		hs.add("glass");
-		hs.add("fork");
-		hs.add("plate");
-		hs.add("spoon");
-		hs.add("knife");
-		hs.add("chair");
-		hs.add("disgusting");
-		hs.add("fresh");
-		hs.add("odor");
-		hs.add("unsanitary");
-		hs.add("gloves");
-		hs.add("eww");
-		hs.add("greasy");
-		hs.add("hot");
-		hs.add("quality");
-		hs.add("ingredient");
-		hs.add("low");
-		hs.add("stale");
-		hs.add("bathroom");
-		hs.add("wall");
-		hs.add("smoke");
-		hs.add("stain");
-		hs.add("undercook");
-		
+	public static void readHygieneDictionary(){
 		try{
-			String outputFile = "data/hygiene_related_reviews.csv";
-			String outputFile1 = "data/hygiene_reviews.csv";
-			// before we open the file check to see if it already exists
-			boolean alreadyExists = new File(outputFile).exists();
-			
-			CsvWriter writer = new CsvWriter(new FileWriter("data/hygiene_related_reviews.csv", true), ',');
-			CsvWriter writer1 = new CsvWriter(new FileWriter("data/hygiene_reviews.csv", true), ',');
-			
-			// if the file didn't already exist then we need to write out the header line
-			if (!alreadyExists)
-			{
-				writer.write("user_id");
-				writer.write("review_id");
-				writer.write("text");
-				writer.write("votes.cool");
-				writer.write("business_id");
-				writer.write("votes.funny");
-				writer.write("stars");
-				writer.write("date");
-				writer.write("votes.useful");
-				writer.endRecord();
-				
-				writer1.write("user_id");
-				writer1.write("review_id");
-				writer1.write("text");
-				writer1.write("votes.cool");
-				writer1.write("business_id");
-				writer1.write("votes.funny");
-				writer1.write("stars");
-				writer1.write("date");
-				writer1.write("votes.useful");
-				writer1.endRecord();
+			FileReader fr = new FileReader("hygiene-dictionary.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line = null;
+			while((line = br.readLine()) != null){
+				hygieneDictionary.add(line)
 			}
-			// else assume that the file already has the correct header line
-			ReviewParser reviewparser = new ReviewParser();
-			List<ReviewData> reviews = reviewparser.readReviews();
-			
-			for(ReviewData review : reviews){
-				String text = review.getText();
-				Iterator itr = hs.iterator();
-				
-				while(itr.hasNext()){
-					
-					Object entry = itr.next();
-					if(text.contains(entry.toString())){
-						writer.write(review.getUserId());
-						writer.write(review.getReviewId());
-						writer.write(text);
-						writer.write(Integer.toString(review.getCoolVotes()));
-						writer.write(review.getBusinessId());
-						writer.write(Integer.toString(review.getFunnyVotes()));
-						writer.write(Integer.toString(review.getStars()));
-						writer.write(review.getDate());
-						writer.write(Integer.toString(review.getUsefulVotes()));
-						writer.endRecord();
+			br.close();
+			fr.close();
+		} catch (Exception e){
+			System.out.println("unable to read hygiene-dictionary");
+		}
+	}
+
+	public static void writeToCsv(CsvWriter writer, ReviewData review, String newReview){
+		writer.write(newReview);
+		writer.write(review.getBusinessId());
+		writer.write(Integer.toString(review.getStars()));
+		writer.write(review.getDate());
+		writer.write(Integer.toString(review.getUsefulVotes()));
+		writer.endRecord();
+	}
+
+	public static void writeToCsv(CsvWriter writer) {
+		writer.write("text");
+		writer.write("business_id");
+		writer.write("stars");
+		writer.write("date");
+		writer.write("votes.useful");
+		writer.endRecord();
+	}
+
+	public static void classifyReviews(CsvWriter writer) {
+		ReviewParser reviewparser = new ReviewParser();
+		List<ReviewData> reviews = reviewparser.readReviews();
+		
+		for(ReviewData review : reviews){
+			String[] sentences = review.getText().split(". ");
+			String newReview = "";
+			boolean isReviewRelated = false;
+
+			for(String sentence : sentences){
+				for(String hygieneWord : hygieneDictionary) {
+					if(sentence.contains(hygieneWord)) {
+						isReviewRelated = true;
+						newReview+=sentence+". "; // build a new review with sentences related to hygiene only
 						break;
 					}
-					else{
-						writer1.write(review.getUserId());
-						writer1.write(review.getReviewId());
-						writer1.write(text);
-						writer1.write(Integer.toString(review.getCoolVotes()));
-						writer1.write(review.getBusinessId());
-						writer1.write(Integer.toString(review.getFunnyVotes()));
-						writer1.write(Integer.toString(review.getStars()));
-						writer1.write(review.getDate());
-						writer1.write(Integer.toString(review.getUsefulVotes()));
-						writer1.endRecord();
-					}
-						
 				}
 			}
+
+			if(isReviewRelated){
+				writeToCsv(writer, review, newReview);
+			} else {
+				int stars = review.getStars();
+				if(stars>=4){
+					writeToCsv(writer, review, review.getText());
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		preprocessData();
+	}
+
+	public static void preprocessData(){
+		readHygieneDictionary();
+		try{
+			CsvWriter writer = new CsvWriter(new FileWriter("data/hygiene_related_reviews.csv", true), ',');
+			writeHeader(writer); //write header to csv
+			classifyReviews(writer); // read reviews, classify them and write to csv
 			writer.close();
-			writer1.close();
-			
 		}
 		catch(Exception e){
 			e.printStackTrace();
